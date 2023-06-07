60c4a7a1bda243ffee77f94bef2357f823533846
EnriAnC <enriqueansa@gmail.com>
2023-06-07 11:46:39 -0400

    changing get method to post method in login

--- routes/usuario.py
+++ routes/usuario.py
@@ -25,7 +25,7 @@ class RouterUsuario(APIRouter):
                 'data': result
             }, status_code=200)
 
-        @self.get("/login", tags=['Usuarios'])
+        @self.POST("/login", tags=['Usuarios'])
         def iniciar_sesion(email: str, password: str, response: Response):
             try:
                 UsuarioRepository.open_transaction()
